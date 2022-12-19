e1d9841c96aba11640dea24e6bd6c9ab077192e4
bimai25 <bimai@ucsd.edu>
2022-12-19 12:36:31 -0800

    12:xx am time edge case fixed

--- parseString.py
+++ parseString.py
@@ -1,8 +1,17 @@
+# parameters: string - datetime object
+# return: string - formatted time in xx:xx am/pm format
+# description: datetime objects are formatted in the following manner:
+# date(YYYY-MM_DD)T(HH:MM:SS), the parseTime functions works by getting substrings
+# relative to the T, and then checking the HH to see what its value is. Datetime objects
+# use military time, so that's why the hour value must be checked to see how to convert to am/pm format
 def parseTime(string):
     t_index = string.find('T')
     toReturn = ""
     am = False
-    if int(string[t_index+1])== 0:
+    if int(string[t_index+1]) == 0 and int(string[t_index+2]) == 0:
+        toReturn += "12"
+        am = True
+    elif int(string[t_index+1])== 0:
         toReturn +=string[t_index+2]
         am = True
     elif int(string[t_index+1:t_index+3]) < 12:
